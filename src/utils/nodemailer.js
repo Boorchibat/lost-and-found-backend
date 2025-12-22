86f3402f270ca6d66dac49a3ae12811873da86c7
Boorchibat <coolboyboorchi@gmail.com>
2025-12-22 16:29:43 -0500

    added tls to nodemailer

--- src/utils/nodemailer.js
+++ src/utils/nodemailer.js
@@ -6,6 +6,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.NODEMAILER_EMAIL,
     pass: process.env.NODEMAILER_PASSWORD,
   },
+  tls: {
+    rejectUnauthorized: false, 
+  },
 });
 
 module.exports = transporter;
