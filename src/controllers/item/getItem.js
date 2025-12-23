cd33ce4a754e604bbb8a3d231c614cc78fa6ddfa
Boorchibat <coolboyboorchi@gmail.com>
2025-12-23 15:49:27 -0500

    fixed getItem error

--- src/controllers/item/getItem.js
+++ src/controllers/item/getItem.js
@@ -11,7 +11,7 @@ const mongoose = require("mongoose");
   try {
     const item = await Item.findById(id);
     if (!item) {
-      return res.status(404).json({ error: "Item not Found" });
+      return res.status(201).json({ message: "Item not found" });
     }
     res.status(200).json(item);
   } catch (error) {
