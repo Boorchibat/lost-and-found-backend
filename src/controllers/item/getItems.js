48f2bca24ff9ba07807b73bff5fedc9bb14d5e71
Boorchibat <coolboyboorchi@gmail.com>
2026-01-07 18:54:02 -0500

    sort from newest to oldest items

--- src/controllers/item/getItems.js
+++ src/controllers/item/getItems.js
@@ -3,7 +3,7 @@ const Item = require("../../schema/item");
 
 const getItems = async (req, res) => {
   try {
-    const items = await Item.find();
+    const items = await Item.find().sort({ createdAt: -1 });
     return res.status(200).json(items);
   } catch (error) {
     return res.status(500).json({ error: error.message });
