06c6271292efee89427aa1f692f4e68bd53a3d4f
Boorchibat <coolboyboorchi@gmail.com>
2026-01-10 14:03:15 -0500

    made it if user deletes their user it deletes all the items and claims made by them

--- src/controllers/user/deleteUser.js
+++ src/controllers/user/deleteUser.js
@@ -1,21 +1,27 @@
 const mongoose = require("mongoose");
 const User = require("../../schema/user");
+const Item = require("../../schema/item"); 
 
 const deleteUser = async (req, res) => {
   const { id } = req.params;
+
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(400).json({ message: "ID is not valid" });
   }
   try {
-    const deleteUser = await User.findByIdAndDelete(id);
-    if (!deleteUser) {
-      res.status(400).json({ message: "User not found" });
+    await Item.deleteMany({ User: id });
+
+    const deletedUser = await User.findByIdAndDelete(id);
+
+    if (!deletedUser) {
+      return res.status(404).json({ message: "User not found" });
     }
+
     return res.status(200).json({
-      message: "User deleted successfully"
+      message: "User and all their items deleted successfully",
     });
   } catch (error) {
-    return res.status(400).json({ message: error.message });
+    return res.status(500).json({ message: error.message });
   }
 };
 
